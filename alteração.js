05b88ac30c9a17582949b3abbced92b4c4bfdaed
maricovp <marcos2015vinicuis@gmail.com>
2024-04-17 11:18:09 -0400

    alterar usando o map

---
+++ alteração.js
@@ -0,0 +1,6 @@
+let personagens = ['batman', 'coringa', 'kratos', 'sonic'];
+let nomesMaiusculos = personagens.map(function(nome){
+    return nome.touppercase();
+});
+console.log(nomesMaiusculos);
+parseFloat
